9ac397e5f8035b938d73f5a1d4b2afda90e17b90
tangshuang <mail@tangshuang.net>
2018-08-29 14:37:06 +0800

    Update async-map.js

--- src/async-map.js
+++ src/async-map.js
@@ -8,19 +8,13 @@ import { $async, $await } from './async-await'
  * @return {array} Promise with new items resolved
  * @example
  * let items = [...]
- * let newItems = await asyncEach(items, async (item, i, items) => {
+ * let newItems = await asyncMap(items, async (item, i, items) => {
  *   let res = await fetch(url, item)
  *   let data = await res.json()
  *   return data
  * })
  */
 export default function asyncMap(items, fn) {
-  if (!Array.isArray(items)) {
-    throw new Error('asyncEach should receive an array as first parameter.')
-  }
-  if (typeof fn !== 'function') {
-    throw new Error('asyncEach should receive a function as the second parameter.')
-  }
   return $await(items, (items) => {
     let promises = []
     let afn = $async(fn)
