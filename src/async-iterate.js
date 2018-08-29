3a673a44c8bfb6a29ea932a6f4102ecf0928a597
tangshuang <mail@tangshuang.net>
2018-08-29 14:36:22 +0800

    Update async-iterate.js

--- src/async-iterate.js
+++ src/async-iterate.js
@@ -16,19 +16,8 @@ import { $async, $await } from './async-await'
  *     stop()
  *   }
  * })
- * 
- * // `next` and `stop` are optional, it is determined by your iterator function
- * await asyncIterate(items, async (item, i) => {
- *   //...
- * })
  */
 export default function asyncIterate(items, fn) {
-  if (!Array.isArray(items)) {
-    throw new Error('asyncIterate should receive an array as first parameter.')
-  }
-  if (typeof fn !== 'function') {
-    throw new Error('asyncIterate should receive a function as the second parameter.')
-  }
   return $await(items, (items) => {
     return new Promise((resolve, reject) => {
       let i = 0
@@ -40,12 +29,7 @@ export default function asyncIterate(items, fn) {
         }
         let afn = $async(fn)
         return new Promise((next, stop) => {
-          if (fn.length > 2) {
-            afn(item, i ++, next, stop)
-          }
-          else {
-            afn(item, i ++).then(next).catch(stop)
-          }
+          afn(item, i ++, next, stop)
         }).then(through).catch(reject)
       }
       through()
